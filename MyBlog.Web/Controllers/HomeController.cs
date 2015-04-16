851f54f365619a52a9ab80e585da6fcdfc04a332
BeingDev <vidwan_20@yahoo.com>
2015-04-16 16:03:42 -0400

    Create CRUD views for Blog Model

--- MyBlog.Web/Controllers/HomeController.cs
+++ MyBlog.Web/Controllers/HomeController.cs
@@ -15,5 +15,46 @@ namespace MyBlog.Web.Controllers
             blogList.Add(new Blog { BlogId = 1, BlogName = "MyBlog2", CreatedBy = "Vidwan", CreatedOn = DateTime.Now, ModifiedBy = "Vidwan", ModifiedOn = DateTime.Now });
             return View(blogList);
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Create(Blog vm)
+        {
+            return View();
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+
+        public ActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Edit(Blog vm)
+        {
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Delete(Blog vm)
+        {
+            return View();
+        }
+
     }
 }
